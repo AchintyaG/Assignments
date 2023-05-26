0dd400ca1e02dd901f5e60b1fced7ea48439bfb2
PRAVEEN KUMAR <117474739+AchintyaG@users.noreply.github.com>
2023-05-26 13:41:30 +0530

    updated files

---
+++ F3 Assignments/9thfeb/script.js
@@ -0,0 +1,38 @@
+var secretNumber = Math.floor(Math.random() * 100) + 1;
+    var previousGuess;
+    var previousDifference;
+    // var btn = getElementById('btn');
+
+    function checkGuess() {
+      var guess = parseInt(document.getElementById('guess').value);
+      var response = document.getElementById('respond');
+
+if (!previousGuess) {
+        previousGuess = guess;
+        previousDifference = Math.abs(guess - secretNumber);
+        response.innerText = 'Guess higher or lower.';
+      } else {
+        var currentDifference = Math.abs(guess - secretNumber);
+        if (currentDifference < previousDifference) {
+          response.innerText = 'Getting hotter.';
+          if (guess < secretNumber) {
+            response.innerText += ' Guess higher.';
+          } else if (guess > secretNumber) {
+            response.innerText += ' Guess lower.';
+          }
+        } else if (currentDifference > previousDifference) {
+          response.innerText = 'Getting colder.';
+          if (guess < secretNumber) {
+            response.innerText += ' Guess higher.';
+          } else if (guess > secretNumber) {
+            response.innerText += ' Guess lower.';
+          }
+        } else {
+          response.innerText = 'No change in temperature.';
+        }
+
+        previousGuess = guess;
+        previousDifference = currentDifference;
+      }
+    }
+    btn.addEventListener('click', checkGuess);
