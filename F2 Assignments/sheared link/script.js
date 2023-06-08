05ae33faffebed34cc7ecb72feab1b6fe4c48a58
PRAVEEN KUMAR <117474739+AchintyaG@users.noreply.github.com>
2023-06-08 16:58:11 +0530

    updated files

---
+++ F2 Assignments/sheared link/script.js
@@ -0,0 +1,26 @@
+// your code here
+var form = document.querySelector("form");
+var h3 =document.querySelector("#url");
+var nameElement = document.querySelector("#name");
+var yearElement= document.querySelector("#year");
+var btn= document.querySelector("#button");
+ function handleSubmit(event) {
+ 	event.preventDefault();
+	 var h3Value= h3.innerText;
+	 var name = nameElement.value;
+	 var year = yearElement.value;
+	 if(name && year)
+	 {
+		 h3Value += '?name=' + name + '&year' + year;
+	 }
+	 else if (!name && year)
+	 {
+		 h3Value += '?year=' + year;
+	 }
+	 else if(name && !year)
+	 {
+		 h3Value+= '?name=' + name;
+	 }
+	 h3.textContent= h3Value;
+ }
+form.addEventListener('Submit' , handleSubmit);
